51cda3ff8816d9efa573e875fc278f3829cdca1f
unknown <jonykadhem@gmail.com>
2026-06-23 10:00:20 +0300

    storing User Choice

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hambaa')
+/*-------------------------------- Constants --------------------------------*/
+
+/*-------------------------------- Variables --------------------------------*/
+
+let userChoice =''
+
+/*------------------------ Cached Element References ------------------------*/
+const choices = document.querySelector('#choices')
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+choices.addEventListener('click', function(event){
+    userChoice = event.target.id
+    console.log('user Choice: ',userChoice)
+})
+
+/*-------------------------------- Functions --------------------------------*/
