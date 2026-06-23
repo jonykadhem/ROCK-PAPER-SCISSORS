7109e07e25f588960126dbb6ddad1ab13c581ac9
unknown <jonykadhem@gmail.com>
2026-06-23 12:10:10 +0300

    final commit

--- app.js
+++ app.js
@@ -4,39 +4,60 @@ const choicesArr = ['rock','paper','scissors']
 
 let userChoice =null
 let pcChoice = ''
-
+let des = ''
+let wen = ''
 /*------------------------ Cached Element References ------------------------*/
 const choices = document.querySelector('#choices')
-const rock = document.querySelector('#dwayne johnson')
-const A4 = document.querySelector('#A4')
+const rock = document.querySelector('#dwayne-johnson')
+const paper = document.querySelector('#A4')
 const scissers = document.querySelector('#scissors')
 const reset = document.querySelector('#reset')
+const result = document.querySelector('#result-display')
+const winLose = document.querySelector('#winlose')
 
 /*----------------------------- Event Listeners -----------------------------*/
 choices.addEventListener('click', function(event){
     let randchoice = Math.floor(Math.random()*3)
     userChoice = event.target.id
 
-        if(userChoice === 'dwayne johnson'){
-            A4.classList.add('hidden')
-            scissers.classList.add('hidden')
-        }if(userChoice === 'A4') {
-            rock.classList.add('hidden')
-            scissers.classList.add('hidden')
-        }if(userChoice === 'scissers'){
-            rock.classList.add('hidden')
-            A4.classList.add('hidden')
+        if(userChoice === 'dwayne-johnson'){
+            A4.setAttribute('disabled', 'disabled')
+            scissers.setAttribute('disabled', 'disabled')
+        }else if(userChoice === 'A4'){
+            rock.setAttribute('disabled', 'disabled')
+            scissers.setAttribute('disabled', 'disabled')
+        }else {
+            rock.setAttribute('disabled', 'disabled')
+            A4.setAttribute('disabled', 'disabled')
         }
 
     pcChoice = choicesArr[randchoice]
-
     
+    if (userChoice === pcChoice) {
+        wen = 'tie'
+    }  else if(userChoice === 'dwayne-johnson' && pcChoice === 'scissors') {
+        wen = 'you won 🎉' 
+        
+        }else if(userChoice === 'A4' && pcChoice === 'dwayne-johnson') {
+            wen = 'you won 🎉' 
+        }else if(userChoice === 'scissors' && pcChoice === 'A4') {
+            wen = 'you won 🎉' 
+        }else if(userChoice === 'dwayne-johnson' && pcChoice === 'A4') {
+            wen = 'you lost 😭'            
+        }else if(userChoice === 'A4' && pcChoice === 'scissors') {
+            wen = 'you lost 😭'            
+        }else if(userChoice === 'scissors' && pcChoice === 'dwayne-johnson') {
+            wen = 'you lost 😭'            
+        }
+        
+        result.textContent = `computer chose: ${pcChoice} & you chose ${userChoice} the results is: ${wen}  `
     console.log('user Choice: ',pcChoice)
 })
 reset.addEventListener('click',function () {
-    A4.classList.remove('hidden')
-    scissers.classList.remove('hidden')
-    rock.classList.remove('hidden')
+    A4.removeAttribute('disabled')
+    scissers.removeAttribute('disabled')
+    rock.removeAttribute('disabled')
+    result.textContent = 'Results go here '
 })
 
 /*-------------------------------- Functions --------------------------------*/
